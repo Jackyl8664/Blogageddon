559d7a766f3eecc4f1f4c1440b90da829111bc92
Michael <mike866431@gmail.com>
2011-10-26 14:10:45 -0700

    added post controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Blog::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+    resources :posts
   # Sample resource route with options:
   #   resources :products do
   #     member do
