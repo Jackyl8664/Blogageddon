91f9af2b78c2518d8b59519db45aaff908646803
Michael <mike866431@gmail.com>
2011-10-26 13:35:31 -0700

    added category

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
+  validates :category, presence: true, associated: true
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111026185925) do
+ActiveRecord::Schema.define(:version => 20111026200948) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
